a17aa9ace44f17c1c5192e3a43d56c14a51dcf41
Fredrik Löfgren <fredrik@lofgren.pp.se>
2015-03-27 15:48:03 +0100

    Added dummy endpoints

--- server/server.js
+++ server/server.js
@@ -11,6 +11,18 @@ app.get('/kalle', function(req, res) {
     res.send('Your email is: ' + req.headers['x-forwarded-email'] + ' and your accname: ' + req.headers['x-forwarded-user']);
 });
 
+app.get('/', function(req, res) {
+	res.send([{"dummyCv":"Dummy CV information 1"},{"dummyCv":"Dummy CV information 2"}]);
+	//Support for search
+	//e.g. GET /employees/me?id=1234
+});
+
+app.get('/me', function(req, res) {
+	res.send({"dummyCv":"Dummy CV information"});
+	//Support for getting a specific attribute
+	//e.g. GET /employees/me?attribute=emial
+});
+
 var server = app.listen(9000, function () {
     console.log('Server started: http://localhost:%s/', server.address().port);
 });
