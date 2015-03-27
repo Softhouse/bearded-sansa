8ffaca82c74c0e948f25553443f5675842a4111f
David Eriksson <david.eriksson@softhouse.se>
2015-03-27 16:24:47 +0100

    /me working kindof

--- server/server.js
+++ server/server.js
@@ -5,30 +5,38 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.get('/kalle', function(req, res) {
+app.use('/docs', express.static(path.join(__dirname, 'dist/')));
+
+app.get('/kalle', function (req, res) {
     console.log("ASD");
 
     res.send('Your email is: ' + req.headers['x-forwarded-email'] + ' and your accname: ' + req.headers['x-forwarded-user']);
 });
 
-app.get('/', function(req, res) {
-	res.send([{"dummyCv":"Dummy CV information 1"},{"dummyCv":"Dummy CV information 2"}]);
-	//Support for search
-	//e.g. GET /employees/me?id=1234
+app.get('/', function (req, res) {
+    res.send([{"dummyCv": "Dummy CV information 1"}, {"dummyCv": "Dummy CV information 2"}]);
+    //Support for search
+    //e.g. GET /employees/me?id=1234
 });
 
-app.get('/me', function(req, res) {
-    backend.get('')
+app.get('/me', function (req, res) {
+    backend.get('http://localhost:3232/employees/?email=' + req.headers['x-forwarded-email'], function (data, response) {
+        if (data.length) {
+            res.send(data[0]);
+        }
+        else {
+            res.status(404);
+            res.send({error: 'Who are you?'});
+        }
+    });
 
-	res.send({"dummyCv":"Dummy CV information"});
-	//Support for getting a specific attribute
-	//e.g. GET /employees/me?attribute=email	
 });
 
-app.get('/:email', function(req, res) {
-	res.send({"dummyCv":"Dummy CV information"});
-	//Support for search email
-	console.log(req.params.email)
+app.get('/:email', function (req, res) {
+    res.send({"dummyCv": "Dummy CV information"});
+    //Support for search
+    //e.g. GET /employees/me?id=1234
+    console.log(req.params.email)
 });
 
 var server = app.listen(9000, function () {
