5b8aceb0c3569733b0fb7f8522de004297b04e61
David Eriksson <david.eriksson@softhouse.se>
2016-03-04 15:37:54 +0100

    Not so verbose anymore

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-env
 mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
-cat $HOME/.ssh/id_rsa
-ssh -vvv -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
+ssh -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
