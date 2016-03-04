0e02592b15b053204f90cff26cafcc13fb309b40
David Eriksson <david.eriksson@softhouse.se>
2016-03-04 13:37:47 +0100

    Try to allocate tty

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
+ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
