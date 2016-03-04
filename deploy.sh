e9abe5cb423b6fb05b99ea682733afced5e49bd0
David Eriksson <david.eriksson@softhouse.se>
2016-03-04 14:13:35 +0100

    Verbose ssh logging

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
 export TERM=dumb
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
+ssh -vvv -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
