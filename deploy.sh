69a84db82ce18248108060ee1b4e1d388071e132
David Eriksson <david.eriksson@softhouse.se>
2016-03-04 15:01:49 +0100

    Disable password auth

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ env
 mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
-export TERM=dumb
-ssh -vvv -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
+ssh -vvv -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
