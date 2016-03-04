9d3a7b4ede0adbff7a07e56b562aca7d8de4847e
David Eriksson <david.eriksson@softhouse.se>
2016-03-04 13:59:54 +0100

    Try TERM=dumb

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ env
 mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
-ssh -T -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
+export TERM=dumb
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
