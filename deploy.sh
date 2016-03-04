65864b62f80d7906d35bcf26012a9f2349390894
David Eriksson <david.eriksson@softhouse.se>
2016-03-04 13:52:48 +0100

    Disable terminal

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ env
 mkdir -p "$HOME/.ssh"
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
 chmod 600 $HOME/.ssh/id_rsa
-ssh -tt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
+ssh -T -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no adminuser@kush.softhouselabs.com 'echo hej'
